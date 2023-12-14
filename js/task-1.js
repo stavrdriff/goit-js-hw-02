1a571300a560f688eedc4ae9eb9810fe770b4658
Tymofii <tymofii.wga@gmail.com>
2023-12-14 14:56:08 +0200

    fix test

--- js/task-1.js
+++ js/task-1.js
@@ -13,7 +13,7 @@ const makeTransaction = (quantity, pricePerDroid, customerCredits) => {
     : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-console.log(makeTransaction('sdfdf', 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
