6b417faa2c0881cb8b9bd332ff5ee0a29e56c0e8
Tymofii <tymofii.wga@gmail.com>
2023-12-14 14:55:14 +0200

    refactored task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,26 +1,19 @@
 'use strict';
 
 const makeTransaction = (quantity, pricePerDroid, customerCredits) => {
+  const testArgFirst = quantity && typeof quantity === 'number' && quantity > 0;
+  const testArgSecond = pricePerDroid && typeof pricePerDroid === 'number' && pricePerDroid > 0;
+  const testArgThird = customerCredits && typeof customerCredits === 'number' && customerCredits > 0;
+  let totalPrice;
 
-  if (!quantity || !pricePerDroid || !customerCredits) {
-    return;
-  }
-  else if (quantity < 0 || pricePerDroid < 0 || customerCredits < 0) {
-    return 'Insufficient funds!'
-  }
-  else {
-    const totalPrice = quantity * pricePerDroid;
-
-    if (totalPrice > customerCredits) {
-      return 'Insufficient funds!'
-    }
-    else {
-      return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-    };
-  };
+  return !testArgFirst || !testArgSecond || !testArgThird
+    ? 'wrong data'
+    : totalPrice = quantity * pricePerDroid && totalPrice > customerCredits
+    ? 'Insufficient funds!'
+    : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction('sdfdf', 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
