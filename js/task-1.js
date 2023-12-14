4db58eed72af1c43fda040d260c299d17effb608
Tymofii <tymofii.wga@gmail.com>
2023-12-14 16:47:46 +0200

    fixed from mentor

--- js/task-1.js
+++ js/task-1.js
@@ -4,13 +4,19 @@ const makeTransaction = (quantity, pricePerDroid, customerCredits) => {
   const testArgFirst = quantity && typeof quantity === 'number' && quantity > 0;
   const testArgSecond = pricePerDroid && typeof pricePerDroid === 'number' && pricePerDroid > 0;
   const testArgThird = customerCredits && typeof customerCredits === 'number' && customerCredits > 0;
-  let totalPrice;
 
-  return !testArgFirst || !testArgSecond || !testArgThird
-    ? 'wrong data'
-    : totalPrice = quantity * pricePerDroid && totalPrice > customerCredits
-    ? 'Insufficient funds!'
-    : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  if (!testArgFirst || !testArgSecond || !testArgThird) {
+    return 'wrong data';
+  }
+  
+  const totalPrice = quantity * pricePerDroid
+  
+  if (totalPrice > customerCredits) {
+    return 'Insufficient funds!';
+  }
+  else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
